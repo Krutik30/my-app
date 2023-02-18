7e3080c0a4253186083819909b0f35feccdabfe9
Krutik Aghera <agherakrutik99@gmail.com>
2023-02-18 22:50:47 +0530

    7

--- src/Apps/7/App7.js
+++ src/Apps/7/App7.js
@@ -1,11 +1,32 @@
-
+import react , {useState} from 'react';
 
 function App7(){
+
+    const [headingText, setHeadingText] = useState("Hello");
+    const [isMouseOver, setMouseOver] = useState(false);
+
+    function handelClick(){
+        setHeadingText("Submitted");
+    }
+
+    function handleMouseOver(){
+        setMouseOver(true);
+    }
+
+    function handleMouseOut(){
+        setMouseOver(false);
+    }
+
     return (
         <div>
             <h1>Hello</h1>
             <input className="d-block" type="text" placeholder="What's your name?" />
-            <button>Submit</button>
+            <button
+                style={{background: isMouseOver? "black" : "white"}}
+                onClick={handelClick}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+            >Submit</button>
         </div>
     )
 }
